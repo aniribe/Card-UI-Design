8dc638dd929faf857e72ec6dd5fabbc8d2ec6398
Irina Belousova <irinabelousova11@gmail.com>
2020-02-28 23:00:00 +0400

    New pictures added, EventListeners added CSS code was modified

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// document.getElementsByClassName("option").addEventListener("click", function() {
-//   this.removeC
-// })
-function myFunction() {
-  document.getElementsByClassName("option").classList.toggle("show");
-}
+document.getElementById("icon-1").addEventListener("click", openClose);
+document.getElementById("icon-2").addEventListener("click", openClose);
+document.getElementById("icon-3").addEventListener("click", openClose);
+document.getElementById("icon-4").addEventListener("click", openClose);
 
-document.ready(function() {
-  document.getElementsByClassName("option").classList.toggle("active");
-});
+function openClose() {
+  let element = document.getElementsByClassName("active");
+
+  if (
+    (element[0] !== this.parentElement.parentElement) &
+    (element[0] !== undefined)
+  ) {
+    element[0].classList.toggle("active");
+  }
+
+  this.parentElement.parentElement.classList.toggle("active");
+}
